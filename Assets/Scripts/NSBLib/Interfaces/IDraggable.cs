c6c7dc564dc5a2a9d29e212abd67d2e46c750540
Dennis Abad <dennis.abad6@gmail.com>
2026-01-03 17:26:53 +1100

    Added interfaces IClickable and IDraggable to NSBLib Added Person with sprite-man

---
+++ Assets/Scripts/NSBLib/Interfaces/IDraggable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Interfaces
+{
+    public interface IDraggable
+    {
+        void OnInitialClick(Vector3 position);
+        void OnDragged(Vector3 position);
+        void OnDragReleased();
+    }
+}
