daa736ef3a21c58c450aabdefc5cf2a083fae533
Dennis Abad <dennis.abad6@gmail.com>
2026-01-03 20:35:27 +1100

    Implemented object pooling

--- Assets/Scripts/Envelope.cs
+++ Assets/Scripts/Envelope.cs
@@ -1,15 +1,35 @@
+using System;
+using System.Collections;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 using Enums;
+using NSBLib.Helpers;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class Envelope : MonoBehaviour
 {
     [SerializeField] private EEnvelopeType envelopeType;
+    [SerializeField] private Transform destTransform;
+    [SerializeField] private float tweenDuration = 0.3f;
+    [SerializeField] private Ease tweenEase = Ease.Flash;
+    [SerializeField] private float lifetime = 1f;
     
+    public Spawner spawner;
+    private Vector2 destCoords =  Vector2.zero;
     private SpriteRenderer spriteRenderer;
+    private Action<Envelope> destroyAction;
+    
+    private TweenerCore<Vector3, Vector3, VectorOptions> envelopeTween;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (destTransform != null)
+        {
+            destCoords = destTransform.position;
+        }
         spriteRenderer = GetComponent<SpriteRenderer>();
         switch (envelopeType)
         {
@@ -24,11 +44,61 @@ public class Envelope : MonoBehaviour
                 spriteRenderer.color = Color.white;
                 break;
         }
+        
+        GoToDestination();
+
+        // StartCoroutine(DelayedDestroy());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Init(Action<Envelope> action)
     {
-        
+        destroyAction = action;
+    }
+
+    IEnumerator DelayedDestroy()
+    {
+        yield return new WaitForSeconds(lifetime);
+        Destroy(gameObject);
+    }
+
+    public void ScheduleDestroy()
+    {
+        StartCoroutine(DelayedDestroy());
+    }
+
+    private void GoToDestination()
+    {
+        envelopeTween = transform.DOMove(destCoords, tweenDuration).SetEase(tweenEase);
+        envelopeTween.OnComplete(() =>
+            {
+                NSBLogger.Log("DONE!");
+            }
+        );
+    }
+
+    public void SetEnvelopeType(EEnvelopeType envelopeType)
+    {
+        this.envelopeType = envelopeType;
+    }
+
+    public void SetEaseType(Ease ease)
+    {
+        tweenEase = ease;
+    }
+
+    public void SetDuration(float f)
+    {
+        tweenDuration = f;
+    }
+
+    public void SetLifetime(float envelopeLifetime)
+    {
+        lifetime = envelopeLifetime;
+    }
+
+    public void SetDesination(Transform destination)
+    {
+        destTransform = destination;
+        destCoords = destTransform.position;
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,23 +1,100 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using Enums;
 using NSBLib.Helpers;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Pool;
+using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] GameObject prefab;
+    [SerializeField] private Envelope prefab;
+    [SerializeField] private float spawnRate = 1f;
+    [SerializeField] private float tweenDuration = .5f;
+    [SerializeField] private float envelopeLifetime = 1f;
+    [SerializeField] private Vector2 spawnPointRange;
+    [SerializeField] private Ease tweenEase = Ease.Flash;
+    public Transform target;
+    [SerializeField] private List<Envelope> objects = new();
 
-    [SerializeField] List<GameObject> objects = new List<GameObject>();
+
+    private ObjectPool<Envelope> pool;
     
     private void Start()
     {
+        pool = new ObjectPool<Envelope>(() =>
+        {
+            return Instantiate(prefab, RandomizePosition(), Quaternion.identity, transform);
+        }, env =>
+        {
+            env.gameObject.SetActive(true);
+        }, env =>
+        {
+            env.gameObject.SetActive(false);
+        }, env =>
+        {
+            Destroy(env.gameObject);
+        }, false, 100, 10000);
         StartCoroutine(Spawn());
     }
 
     IEnumerator Spawn()
     {
-        NSBLogger.Log("Hello every 5secs");
-        yield return new WaitForSeconds(5f);
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            // Envelope envelope = Instantiate(prefab, RandomizePosition(), Quaternion.identity, transform);
+            Envelope envelope = pool.Get();
+            RandomizeEnvelope(envelope);
+            NSBLogger.Log($"Spawned at: {envelope.transform.position}");
+            // AddObject(envelope);
+        }
+    }
+
+    private void RandomizeEnvelope(Envelope envelope)
+    {
+        envelope.SetEnvelopeType((EEnvelopeType)Random.Range(0, 3));
+        envelope.SetEaseType(tweenEase);
+        envelope.SetDuration(tweenDuration);
+        envelope.SetLifetime(envelopeLifetime);
+        envelope.SetDesination(target);
+        envelope.ScheduleDestroy();
+    }
+
+    private Vector3 RandomizePosition()
+    {
+        var xPos =  Random.Range(0f, spawnPointRange.x);
+        var yPos = Random.Range(0f, spawnPointRange.y);
+
+        xPos = SetPolarity(xPos);
+        yPos = SetPolarity(yPos);
+        
+        return  new Vector3(xPos + transform.position.x, yPos + transform.position.y, 0f);
+    }
+
+    private float SetPolarity(float pos)
+    {
+        switch (Random.Range(1, 3))
+        {
+            case 1:
+                pos *= -1f;
+                break;
+            case 2:
+                pos *= 1f;
+                break;
+        }
+        return pos;
+    }
+
+    private void AddObject(Envelope obj)
+    {
+        objects.Add(obj);
+    }
+
+    public void RemoveEnvelope(Envelope envelope)
+    {
+        objects.Remove(envelope);
     }
 }
