fd86915d843381ea2925796b59c4b91593854cf5
Dennis Abad <dennis.abad6@gmail.com>
2026-01-04 23:20:17 +1100

    Added slotGroups in Table for easier setup of predefined slots coordinates

--- Assets/Scripts/Table.cs
+++ Assets/Scripts/Table.cs
@@ -9,6 +9,8 @@ public class Table : MonoBehaviour
 {
     [SerializeField] TableEnvelope[] envelopes;
     [SerializeField] private TableEnvelope prefab;
+    [SerializeField] private Transform slotGroup;
+    [SerializeField] private bool hasGuide;
     private List<Transform> slotList;
 
     private void OnEnable()
@@ -19,13 +21,27 @@ public class Table : MonoBehaviour
 
     private void SetupSlots()
     {
-        var transforms = GetComponentsInChildren<Transform>();
+        SetupVisualGuide();
+        
+        var transforms = slotGroup.GetComponentsInChildren<Transform>();
         var slotTransforms = transforms.ToList();
         // Remove first transform since it's the transform of the table
         slotTransforms.RemoveAt(0);
         slotList = slotTransforms;
 
         NSBLogger.Log($"{slotList.Count}");
+    }
+
+    private void SetupVisualGuide()
+    {
+        if (!hasGuide)
+        {
+            var renderers = slotGroup.GetComponentsInChildren<SpriteRenderer>();
+            foreach (var r in renderers)
+            {
+                r.enabled = false;
+            }
+        }
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -38,8 +54,10 @@ public class Table : MonoBehaviour
     {
         for (int i = 0; i < envelopes.Length; i++)
         {
-            yield return new WaitForSeconds(0.5f);
-            var newEnv =  Instantiate(prefab, slotList[i]);
+            yield return new WaitForSeconds(0.1f);
+            var newEnv =  Instantiate(prefab, slotList[i], true);
+            newEnv.transform.position = new  Vector3(slotList[i].transform.position.x, slotList[i].transform.position.y, slotList[i].transform.position.z -1);
+            
             envelopes[i] = newEnv;
         }
     }
