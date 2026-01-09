095afe2ec676cdcf21df8e3f9c5001802f786270
Dennis Abad <dennis.abad6@gmail.com>
2026-01-09 21:49:16 +1100

    Updated Card object to implement IRightClickable

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -6,7 +6,7 @@ using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
-public class Card : MonoBehaviour, IClickable
+public class Card : MonoBehaviour, IClickable, IRightClickable
 {
     public int marketPrice;
     public int askingPrice;
