23ea6543e77d4ed7abca5b683ee65a78e0ff7c3f
Dennis Abad <dennis.abad6@gmail.com>
2026-01-21 21:08:08 +1100

    Moved Clickable with Cooldown to its own script for decoupling

---
+++ Assets/Scripts/NSBLib/Components/ClickableWithCooldown.cs
@@ -0,0 +1,61 @@
+﻿using NSBLib.EventChannelSystem;
+using NSBLib.Interfaces;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace NSBLib.Components
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class ClickableWithCooldown: MonoBehaviour, IClickable
+    {
+        [SerializeField] private float timer;
+        [SerializeField] private float duration = 5f;
+        public float percent = 0f;
+
+        [SerializeField] private UnityEvent OnClick;
+        [SerializeField] private UnityEvent<float> OnTimerUpdate;
+
+        // private float percent = 0f;
+        private bool cooldownDone = true;
+        
+        [SerializeField] private EventChannel<GameObject> OnCooldownFinished;
+        
+        private void FixedUpdate()
+        {
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+                percent = timer / duration;
+                OnTimerUpdate?.Invoke(percent);
+
+                if (timer <= 0)
+                {
+                    timer = 0;
+                    OnCooldownFinished?.Invoke(gameObject);
+                    cooldownDone = true;
+                }
+                else
+                {
+                    cooldownDone = false;
+                }
+            }
+        }
+
+        public void OnClicked()
+        {
+            Debug.Log("Clicked");
+            if (cooldownDone)
+            {
+                SetCooldown();
+                OnClick?.Invoke();
+            }
+        }
+    
+        private void SetCooldown()
+        {
+            Debug.Log("SetCooldown");
+            cooldownDone = false;
+            timer = duration;
+        }
+    }
+}
--- Assets/Scripts/Scene2/RedCard.cs
+++ Assets/Scripts/Scene2/RedCard.cs
@@ -6,63 +6,23 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
 
-public class RedCard : MonoBehaviour, IClickable
+public class RedCard : MonoBehaviour
 {
-
-    [SerializeField] private float timer;
-    [SerializeField] private float duration = 5f;
-    
     [SerializeField] private Image cooldownImage;
 
-    // private float percent = 0f;
-    private bool cooldownDone = true;
-
     [SerializeField] private IntEventChannel OnAddPoints;
-    [SerializeField] private EventChannel<GameObject> OnCooldownFinished;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    private void FixedUpdate()
-    {
-        if (timer > 0)
-        {
-            timer -= Time.deltaTime;
-            UpdateCooldownUI(timer / duration);
-
-            if (timer <= 0)
-            {
-                timer = 0;
-                OnCooldownFinished?.Invoke(gameObject);
-                cooldownDone = true;
-            }
-            else
-            {
-                cooldownDone = false;
-            }
-        }
-    }
-
     public void OnClicked()
     {
-        Debug.Log("Clicked");
-        if (cooldownDone)
-        {
-            OnAddPoints.Invoke(1);
-            SetCooldown();
-        }
-    }
-    
-    private void SetCooldown()
-    {
-        Debug.Log("SetCooldown");
-        cooldownDone = false;
-        timer = duration;
+        OnAddPoints.Invoke(1);
     }
 
-    private void UpdateCooldownUI(float p)
+    public void UpdateCooldownUI(float p)
     {
         cooldownImage.fillAmount = p;
     }
